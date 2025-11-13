5ba77dc49715ccb7372a450af0cdabd9e7238522
Dineth <dinethfiverr23@gmail.com>
2025-11-13 20:51:02 +0530

    Add Static Page Paths

---
+++ apps/client/app/(public)/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
+
+    return (
+
+        <div className="min-h-screen bg-white text-gray-900">
+
+            {children}
+
+        </div>
+
+    );
+
+}
