b44afcbb8378a0a25e977aab9bfe9192bf7150f0
Dineth <dinethfiverr23@gmail.com>
2025-11-24 15:35:28 +0530

    Create Use DashBoard Hook

--- apps/client/modules/dashboard/dashboard-data/page.tsx
+++ apps/client/modules/dashboard/dashboard-data/page.tsx
@@ -1,8 +1,17 @@
+'use client'
+
 import React from 'react'
+import { useDashboardData } from './hooks/useDashBoardData';
 
 const DashBoardPage = () => {
+
+    const { data, isLoading, isError, refetch } = useDashboardData();
+
     return (
-        <div>DashBoardPage</div>
+        <>
+            <div>DashBoardPage</div>
+            <p>{isLoading ? 'Loading...' : isError ? 'Error loading data' : JSON.stringify(data)}</p>
+        </>
     )
 }
 
