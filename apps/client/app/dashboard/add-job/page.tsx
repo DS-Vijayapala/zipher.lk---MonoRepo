c476173ab07671e519fe18ec6cae18a705136b97
Dineth <dinethfiverr23@gmail.com>
2025-11-25 14:23:41 +0530

    Ready For Development Add Job Page

---
+++ apps/client/app/dashboard/add-job/page.tsx
@@ -0,0 +1,8 @@
+import AddJob from '@/modules/dashboard/add-job/page'
+import React from 'react'
+
+const page = () => {
+    return <AddJob />
+}
+
+export default page
---
+++ apps/client/modules/dashboard/add-job/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+
+import React from 'react'
+
+const AddJob = () => {
+    return (
+
+        <div className='max-w-6xl mx-auto px-4 py-8'>Add Jobs</div>
+    )
+}
+
+export default AddJob
