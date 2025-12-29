5fcca693bf41036ea3d84d67d3361f36bdf67f21
Dineth <dinethfiverr23@gmail.com>
2025-12-29 13:35:28 +0530

    Ready Manage Jobs Page For Development

---
+++ apps/client/app/dashboard/manage-jobs/page.tsx
@@ -0,0 +1,8 @@
+import ManageJobs from '@/modules/dashboard/manage-jobs/page'
+import React from 'react'
+
+const page = () => {
+    return <ManageJobs />
+}
+
+export default page
---
+++ apps/client/modules/dashboard/manage-jobs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ManageJobs = () => {
+    return (
+        <div className='container max-w-6xl mx-auto px-4 py-8 space-y-8'>
+            ManageJobs
+        </div>
+    )
+}
+
+export default ManageJobs
